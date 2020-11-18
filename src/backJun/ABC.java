1822d65fcd9fdec40c14128fc10c22e610de01b7
x12323 <x12323@naver.com>
2020-11-18 23:28:38 +0900

    집

---
+++ src/backJun/ABC.java
@@ -0,0 +1,17 @@
+package backJun;
+
+import java.util.Scanner;
+
+public class ABC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
